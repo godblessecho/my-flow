06d5cc0f2a6479eeaafd8c0a9abd4d403c9842e1
godblessecho <55050710+godblessecho@users.noreply.github.com>
2025-12-27 17:54:33 +0800

    Add files via upload
    update icon

--- sw.js
+++ sw.js
@@ -1,31 +1,26 @@
-const CACHE_NAME = 'flow-glow-v13.0'; // 升级个大版本号！
+const CACHE_NAME = 'flow-glow-final-v99'; // 版本号改得超大！
 
-// 需要缓存的文件列表
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
   './manifest.json',
-  './icon-purple.png'
+  './logo.png' // 名字改了
 ];
 
-// 安装事件：缓存核心资源
+// 下面的代码保持不变...
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      console.log('Opened cache');
       return cache.addAll(ASSETS_TO_CACHE);
     })
   );
 });
-
-// 激活事件：清理旧版本的缓存
 self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((cacheNames) => {
       return Promise.all(
         cacheNames.map((cacheName) => {
           if (cacheName !== CACHE_NAME) {
-            console.log('Deleting old cache:', cacheName);
             return caches.delete(cacheName);
           }
         })
@@ -33,17 +28,10 @@ self.addEventListener('activate', (event) => {
     })
   );
 });
-
-// 请求拦截：优先使用缓存，没有缓存则请求网络
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
-      // 如果缓存中有，直接返回
-      if (response) {
-        return response;
-      }
-      // 否则发起网络请求
-      return fetch(event.request);
+      return response || fetch(event.request);
     })
   );
 });
